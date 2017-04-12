3d154f5f34216de63a905caf61ed5364b23e4a17
tanhien0809 <hien@Hiens-Mac.local>
2017-04-12 22:23:51 +0700

    up

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Animations
 //
-//  Created by Daniel Autenrieth on 07/10/2016.
+//  Created by Brian Advent on 07/10/2016.
 //  Copyright © 2016 Brian Advent. All rights reserved.
 //
 
@@ -10,12 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var button2: NSLayoutConstraint!
+    
+    @IBOutlet weak var button1: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        button1.constant -= view.bounds.width
+        button2.constant -= view.bounds.width
+    }
+    
+    var animationPerformedOnce = false
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if !animationPerformedOnce
+        {
+            UIView.animate(withDuration: 1, delay: 0, options: .curveEaseOut, animations: {
+                self.button1.constant += self.view.bounds.width
+                self.view.layoutIfNeeded()
+            }, completion: nil)
+            UIView.animate(withDuration: 1, delay: 0, options: .curveEaseOut, animations: {
+                self.button2.constant += self.view.bounds.width
+                self.view.layoutIfNeeded()
+            }, completion: nil)
+            animationPerformedOnce = true
+        }
     }
 
-    override func didReceiveMemoryWarning() {
+       override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
